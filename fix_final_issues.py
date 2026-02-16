eb878facf6391d2d770be23bf0eb1e380730855a
dianchen0225-wq <dianchen0225@gmail.com>
2026-02-16 18:52:48 +0800

    初始化AstroRead项目，提交基础代码

--- fix_final_issues.py
+++ fix_final_issues.py
@@ -1,3 +1,128 @@
+> hvigor DEBUG hvigor-worker-pool default@CompileArkTS work[1] failed.
+> hvigor ERROR: Failed :entry:default@CompileArkTS... 
+> hvigor WARN: 
+1 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SearchPage.ets:96:22
+ 'showDialog' has been deprecated.
+
+2 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SearchPage.ets:171:22
+ 'showDialog' has been deprecated.
+
+3 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SearchPage.ets:187:22
+ 'showDialog' has been deprecated.
+
+4 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SearchPage.ets:205:14
+ 'push' has been deprecated.
+
+5 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookshelfPage.ets:245:24
+ 'push' has been deprecated.
+
+6 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:111:22
+ 'showDialog' has been deprecated.
+
+7 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:181:22
+ 'showDialog' has been deprecated.
+
+8 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:201:22
+ 'showDialog' has been deprecated.
+
+9 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:252:24
+ 'showDialog' has been deprecated.
+
+10 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:277:26
+ 'showDialog' has been deprecated.
+
+11 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:297:24
+ 'showDialog' has been deprecated.
+
+12 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:308:24
+ 'showDialog' has been deprecated.
+
+13 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:319:24
+ 'showDialog' has been deprecated.
+
+14 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:342:22
+ 'showDialog' has been deprecated.
+
+15 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:372:24
+ 'showDialog' has been deprecated.
+
+16 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:389:24
+ 'showDialog' has been deprecated.
+
+17 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:405:22
+ 'showDialog' has been deprecated.
+
+18 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:433:20
+ 'showDialog' has been deprecated.
+
+19 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:486:28
+ 'showDialog' has been deprecated.
+
+20 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:510:30
+ 'showDialog' has been deprecated.
+
+21 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:527:26
+ 'showDialog' has been deprecated.
+
+22 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:553:22
+ 'showDialog' has been deprecated.
+
+23 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:611:22
+ 'showDialog' has been deprecated.
+
+24 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:627:22
+ 'showDialog' has been deprecated.
+
+25 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:666:24
+ 'showDialog' has been deprecated.
+
+26 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:693:22
+ 'showDialog' has been deprecated.
+
+27 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:710:22
+ 'showDialog' has been deprecated.
+
+28 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:734:24
+ 'showDialog' has been deprecated.
+
+29 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:750:24
+ 'showDialog' has been deprecated.
+
+30 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/SourcePage.ets:766:22
+ 'showDialog' has been deprecated.
+
+31 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/ReadPage.ets:93:27
+ 'getParams' has been deprecated.
+
+32 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/ReadPage.ets:151:24
+ 'showDialog' has been deprecated.
+
+33 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/ReadPage.ets:179:22
+ 'showDialog' has been deprecated.
+
+34 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/ReadPage.ets:202:22
+ 'showDialog' has been deprecated.
+
+35 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/ReadPage.ets:307:22
+ 'back' has been deprecated.
+
+36 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookDetailPage.ets:72:27
+ 'getParams' has been deprecated.
+
+37 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookDetailPage.ets:138:22
+ 'showDialog' has been deprecated.
+
+38 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookDetailPage.ets:154:22
+ 'showDialog' has been deprecated.
+
+39 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookDetailPage.ets:173:22
+ 'showDialog' has been deprecated.
+
+40 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookDetailPage.ets:191:14
+ 'push' has been deprecated.
+
+41 WARN: ArkTS:WARN File: F:/MyApplication/AstroRead/entry/src/main/ets/pages/BookDetailPage.ets:396:30
+ 'push' has been deprecated.
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
